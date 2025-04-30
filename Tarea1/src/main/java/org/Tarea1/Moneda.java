b170460e456e0c8a425d55a5845afb2cf71bc967
Canito301 <franfuentealba2016@gmail.com>
2025-04-30 17:43:38 -0400

    Implementación Comparable clase Moneda, además del toString, que implementa el hashcode y el valor de la moneda.

--- Tarea1/src/main/java/org/Tarea1/Moneda.java
+++ Tarea1/src/main/java/org/Tarea1/Moneda.java
@@ -1,10 +1,21 @@
 package org.Tarea1;
 
-public abstract class Moneda{
+import java.io.StringReader;
+
+public abstract class Moneda implements Comparable<Moneda>{
     public Moneda(){}
     public Moneda getSerie(){
         return this;
     }
+    public int compareTo(Moneda otraMoneda) {
+        return Integer.compare(this.getValor(), otraMoneda.getValor());
+    }
+
+    @Override
+    public String toString() {
+        return "Número de Serie=" + this.hashCode() + " " + getValor() + "pesos chilenos";
+    }
+
     public abstract int getValor();
 }
 
