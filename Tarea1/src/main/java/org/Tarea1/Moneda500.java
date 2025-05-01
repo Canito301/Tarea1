63c0d09abacc92ec75c84b1d93740ecc5f90a72a
Canito301 <franfuentealba2016@gmail.com>
2025-04-30 20:21:41 -0400

    Documentación tipo Javadoc en clase Moneda, Moneda100, Moneda500, Moneda1000.

--- Tarea1/src/main/java/org/Tarea1/Moneda500.java
+++ Tarea1/src/main/java/org/Tarea1/Moneda500.java
@@ -1,9 +1,25 @@
 package org.Tarea1;
-
+/**
+ * Representa una moneda chilena de 500 pesos.
+ * <p>
+ * Esta clase extiende a {@link Moneda} y define el valor específico
+ * de la moneda como 500 pesos mediante la implementación del método {@link #getValor()}.
+ * </p>
+ */
 public class Moneda500 extends Moneda{
+
+    /**
+     * Constructor que llama al constructor de {@link Moneda}.
+     */
     public Moneda500(){
         super();
     }
+    /**
+     * Retorna el valor de esta moneda.
+     *
+     * @return el valor 500, que corresponde a 500 pesos chilenos.
+     */
+    @Override
     public int getValor(){
         return 500;
     }
